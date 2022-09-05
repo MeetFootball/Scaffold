20902a906d1672b084b286572973e4e26c56189f
Raven <raven.259695@gmail.com>
2022-09-05 12:40:39 +0800

    优化SQL 日志

--- app/mysql.go
+++ app/mysql.go
@@ -3,12 +3,9 @@ package app
 import (
 	"errors"
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/MeetFootball/Scaffold/crontab"
-	"github.com/MeetFootball/Scaffold/model"
 	"github.com/MeetFootball/Scaffold/util"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -25,52 +22,6 @@ type MySQLConfig struct {
 
 type MySQLMapConfig struct {
 	List map[string]*MySQLConfig `mapstructure:"list"`
-}
-
-// Writer 数据库日志输出
-type Writer struct{}
-
-func (w Writer) Printf(f string, args ...interface{}) {
-	const (
-		LocationIndex = iota
-		CostIndex
-		RowsIndex
-		SQLIndex
-	)
-	var (
-		ok, slow         bool
-		cost             float64
-		rows             uint64
-		location, t, SQL string
-	)
-	Log := &model.SQLLog{}
-	t = "logs"
-	ErrorIndex := 0
-	if len(args) == 5 {
-		ErrorIndex = 1
-		Log.Error = fmt.Sprintf("%s", args[ErrorIndex])
-		t = "errors"
-	}
-	if location, ok = args[LocationIndex].(string); ok {
-		Log.Location = location
-	}
-	if cost, ok = args[CostIndex+ErrorIndex].(float64); ok {
-		Log.Cost = cost
-		if cost > 2000 {
-			slow = true
-			t = "slow"
-		}
-		Log.Slow = slow
-	}
-	if rows, ok = args[RowsIndex+ErrorIndex].(uint64); ok {
-		Log.Rows = rows
-	}
-	if SQL, ok = args[SQLIndex+ErrorIndex].(string); ok {
-		Log.SQL = SQL
-	}
-	Log.Type = t
-	Log.Tag = map[string]string{"slow": strconv.FormatBool(slow), "type": t}
-	crontab.SQLLogChan <- Log
 }
 
 var MySQLPool map[string]*gorm.DB
---
+++ app/sql_writer.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/MeetFootball/Scaffold/crontab"
+	"github.com/MeetFootball/Scaffold/model"
+)
+
+// Writer 数据库日志输出
+type Writer struct{}
+
+func (w Writer) Printf(f string, args ...interface{}) {
+	const (
+		LocationIndex = iota
+		CostIndex
+		RowsIndex
+		SQLIndex
+	)
+	var (
+		ok, slow         bool
+		cost             float64
+		rows             uint64
+		location, t, SQL string
+	)
+	Log := &model.SQLLog{}
+	t = "logs"
+	ErrorIndex := 0
+	if len(args) == 5 {
+		ErrorIndex = 1
+		Log.Error = fmt.Sprintf("%s", args[ErrorIndex])
+		t = "errors"
+	}
+	if location, ok = args[LocationIndex].(string); ok {
+		Log.Location = location
+	}
+	if cost, ok = args[CostIndex+ErrorIndex].(float64); ok {
+		Log.Cost = cost
+		if cost > 200 {
+			slow = true
+			t = "slow"
+		}
+		Log.Slow = slow
+	}
+	if rows, ok = args[RowsIndex+ErrorIndex].(uint64); ok {
+		Log.Rows = rows
+	}
+	if SQL, ok = args[SQLIndex+ErrorIndex].(string); ok {
+		Log.SQL = SQL
+	}
+	Log.Type = t
+	Log.Tag = map[string]string{"slow": strconv.FormatBool(slow), "type": t}
+	crontab.SQLLogChan <- Log
+}
